efb3a1ca861baf13278ebc004a57414c9ac4fbd3
wl01469740 <black123963@gmail.com>
2022-07-01 14:25:40 +0800

    0.2.0(0701首頁手機板)

---
+++ public/js/easywash-script.js
@@ -0,0 +1,26 @@
+//===============================
+// 手機板 PC板 menu nav
+//========================
+// 定義視窗寬度的變數
+let windowWidth = $(window).width()
+
+if( windowWidth >= 768){
+    //大於等於768執行
+
+    //$('nav>ul>li').hover(functiom(){}, function(){})
+    $('nav>ul>li').hover(function(){
+        //滑鼠滑入執行
+        $(this),find('.submenu').addClass('active')
+    }, function(){
+        //滑鼠滑出執行
+        $(this),find('.submenu').removeClass('active')
+    })
+}else{
+    //小於768執行
+    $('#btn-nav-switch').on('click', function(){
+        $('nav').toggleClass('active')
+    })// btn-nav-switch end
+    $('nav>ul>li').on('click', function(){
+        $('.submenu').toggleClass('active')
+    })// nav>ul>li end
+}//大於等於768執行 end
